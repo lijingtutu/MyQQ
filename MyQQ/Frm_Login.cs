8254fb3ba8e670f6cd47eac93900d713725a447d
LiJing <1091955237@qq.com>
2018-07-29 12:22:01 +0800

    登录界面

---
+++ MyQQ/Frm_Login.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyQQ
+{
+    public partial class Frm_Login : Form
+    {
+        public Frm_Login()
+        {
+            InitializeComponent();
+        }
+        private bool ValidateInput()
+        {
+            if(txtID.Text.Trim()=="")
+            {
+                MessageBox.Show("请输入登录账号", "登录提示", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Information);
+                txtID.Focus();
+                return false;
+
+            }
+            else if (int.Parse(txtID.Text.Trim())>65535)
+            {
+                MessageBox.Show("请输入正确的登录账号", "登录提示", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                txtID.Focus();
+                return false;
+
+            }
+            else if (txtID.Text.Length>5 && txtPwd.Text.Trim()=="")
+            {
+                MessageBox.Show("请输入密码", "登录提示", MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                txtPwd.Focus();
+                return false;
+
+            }
+            return true;
+        }
+        private void txtID_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(char.IsDigit(e.KeyChar) || (e.KeyChar=='\r') || (e.KeyChar=='\b'))
+            {
+                e.Handled = false;
+            }
+            else
+                e.Handled = true;
+        }
+    }
+}
--- MyQQ/Program.cs
+++ MyQQ/Program.cs
@@ -15,7 +15,7 @@ namespace MyQQ
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Frm_Login());
         }
     }
 }
